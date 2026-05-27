f5f5b76ba7ef04098554bae7d81af660ed02b5ee
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-27 14:21:22 +0300

    Update filtering.sql

--- 03.DQL/filtering.sql
+++ 03.DQL/filtering.sql
@@ -1,1 +1,54 @@
+-- 1. Display customers from Cluj.
+SELECT *
+FROM Customers
+WHERE City = 'Cluj';
+
+
+-- 2. Display customers older than 28.
+SELECT *
+FROM Customers
+WHERE Age > 28;
+
+
+-- 3. Display orders with a price greater than or equal to 1000.
+SELECT *
+FROM Orders
+WHERE Price >= 1000;
+
+
+-- 4. Display customers: from Cluj and younger than 30.
+SELECT *
+FROM Customers
+WHERE City = 'Cluj' AND Age < 30;
+
+
+-- 5. Display customers: from Cluj or from Iasi.
+SELECT *
+FROM Customers
+WHERE City = 'Cluj' OR City = 'Iasi';
+
+
+-- 6. Display customers who are NOT from Bucuresti.
+SELECT *
+FROM Customers
+WHERE City != 'Bucuresti';
+
+
+-- 7. Display customers aged between 25 and 35.
+SELECT *
+FROM Customers
+WHERE Age BETWEEN 25 AND 35;
+
+
+-- 8. Display customers from: Cluj, Iasi, Timisoara.
+SELECT *
+FROM Customers
+WHERE City IN ('Cluj', 'Iasi', 'Timisoara');
+
+
+-- 9. Display customers whose first name starts with the letter A.
+SELECT *
+FROM Customers
+WHERE FirstName LIKE 'A%';
+
 
