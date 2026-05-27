73eeb94127854722cd6e4ce3658d2bbecabab050
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-27 14:14:14 +0300

    Update select.sql

--- 03.DQL/select.sql
+++ 03.DQL/select.sql
@@ -1,1 +1,15 @@
+-- 1. Display all customers.
+SELECT *
+FROM Customers;
 
+
+/* 2. Display: FirstName, City for all customers.  */
+SELECT
+    FirstName,
+    City
+FROM Customers;
+
+
+/* 3. Display the first three orders.  */
+SELECT TOP 3 *
+FROM Orders;
