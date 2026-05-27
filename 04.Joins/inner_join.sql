2e001c1500d1934ebaa6edd7dacdde782d551482
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-27 14:39:26 +0300

    Update inner_join.sql

--- 04.Joins/inner_join.sql
+++ 04.Joins/inner_join.sql
@@ -1,1 +1,32 @@
+/* 1. Display: the customer's first name and the ordered product.
+Only for orders with a valid customer. */
+SELECT
+    c.FirstName,
+    o.ProductName
+FROM Customers AS c
+INNER JOIN Orders AS o
+ON c.CustomerID = o.CustomerID;
+
+
+/* 2. Display: the customer's first name, the product and the price.
+Ordered descending by price. */
+SELECT
+    c.FirstName,
+    o.ProductName,
+    o.Price
+FROM Customers AS c
+INNER JOIN Orders AS o
+ON c.CustomerID = o.CustomerID
+ORDER BY Price DESC;
+
+
+/* 3. Display: the customer's first name and the product.
+Only for products containing the letter "o". */
+SELECT
+    c.FirstName,
+    o.ProductName
+FROM Customers AS c
+INNER JOIN Orders AS o
+ON c.CustomerID = o.CustomerID
+WHERE ProductName LIKE '%o%';
 
