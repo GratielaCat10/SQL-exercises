ac91a470ad7abdfc1c36ae8553291ce380ea8fe7
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-27 14:57:23 +0300

    Update cross_join.sql

--- 04.Joins/cross_join.sql
+++ 04.Joins/cross_join.sql
@@ -1,1 +1,7 @@
-
+/* 1. Display: the customer's first name and the product name.
+For all possible combinations. */
+SELECT
+    c.FirstName,
+    o.ProductName
+FROM Customers AS c
+CROSS JOIN Orders AS o;
