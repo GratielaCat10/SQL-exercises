d38bf8328a35793b0d1e1800c543483115ef9124
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-27 15:02:56 +0300

    Update cross_join.sql

--- 04.Joins/cross_join.sql
+++ 04.Joins/cross_join.sql
@@ -5,3 +5,18 @@ SELECT
     o.ProductName
 FROM Customers AS c
 CROSS JOIN Orders AS o;
+
+
+/* 2. Display: the customer's first name, the customer's city, the ordered product and the price.
+For all possible combinations between: customers and orders.
+Show only orders with a price greater than 1000.
+Ordered descending by price.  */
+SELECT
+    c.FirstName,
+    c.City,
+    o.ProductName,
+    o.Price
+FROM Customers AS c
+CROSS JOIN Orders AS o
+WHERE o.Price > 1000
+ORDER BY o.Price DESC;
