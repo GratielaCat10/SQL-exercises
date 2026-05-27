7270c795f377fe9235878a1356691c15a49362bf
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-27 14:05:30 +0300

    Update insert.sql

--- 02.DML/insert.sql
+++ 02.DML/insert.sql
@@ -1,1 +1,18 @@
+-- Insert data in Customers table
+INSERT INTO Customers
+VALUES
+(1, 'Andrei', 'Popescu', 'Cluj', 25),
+(2, 'Maria', 'Ionescu', 'Bucuresti', 31),
+(3, 'Alex', 'Georgescu', 'Cluj', 22),
+(4, 'Elena', 'Matei', 'Iasi', 29),
+(5, 'David', 'Radu', 'Timisoara', 35);
 
+
+--Insert data in Orders table
+INSERT INTO Orders
+VALUES
+(101, 1, 'Laptop', 1, 4500),
+(102, 1, 'Mouse', 2, 150),
+(103, 2, 'Phone', 1, 3200),
+(104, 3, 'Keyboard', 1, 250),
+(105, 6, 'Monitor', 2, 1200);
