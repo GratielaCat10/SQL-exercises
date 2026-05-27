310317b9b1b2ced06447e88b005840246b648e5c
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-27 14:00:11 +0300

    Update create_table.sql

--- 01.DDL/create_table.sql
+++ 01.DDL/create_table.sql
@@ -1,1 +1,19 @@
+-- Create the Customers table
+CREATE TABLE Customers (
+    CustomerID INT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    City VARCHAR(50),
+    Age INT
+);
+
+
+-- Create the Orders table
+CREATE TABLE Orders (
+    OrderID INT,
+    CustomerID INT,
+    ProductName VARCHAR(50),
+    Quantity INT,
+    Price DECIMAL(10,2)
+);
 
