c1a75d9141243cbfd148fe8e0161147c115ed31b
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-27 14:46:11 +0300

    Update full_join.sql

--- 04.Joins/full_join.sql
+++ 04.Joins/full_join.sql
@@ -1,1 +1,16 @@
+/* 1. Display: all customers and all orders, even if there is no match. */
+SELECT *
+FROM Customers AS c
+FULL JOIN Orders AS o
+ON c.CustomerID = o.CustomerID;
+
+
+/* 2. Display: the customer's first name, the product and the price, including orders without a valid customer. */
+SELECT
+    c.FirstName,
+    o.ProductName,
+    o.Price
+FROM Customers AS c
+FULL JOIN Orders AS o
+ON c.CustomerID = o.CustomerID;
 
