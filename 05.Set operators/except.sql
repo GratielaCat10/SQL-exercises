6663d14ac94aa9ecc7c70fa473cfcde291104ee9
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-28 11:05:48 +0300

    Update except.sql

--- 05.Set operators/except.sql
+++ 05.Set operators/except.sql
@@ -1,15 +1,15 @@
 -- Customers who are not Employees
 SELECT FirstName, LastName
-FROM Customers
+FROM Sales.Customers
 EXCEPT
 SELECT FirstName, LastName
-FROM Employees;
+FROM Sales.Employees;
 
 
 -- Employees who are not Customers
 SELECT FirstName, LastName
-FROM Employees
+FROM Sales.Employees
 EXCEPT
 SELECT FirstName, LastName
-FROM Customers;
+FROM Sales.Customers;
 
